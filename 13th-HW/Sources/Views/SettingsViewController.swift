4b7191165014fd690504ab7494de0beaf0e52866
Maxim Soloboev <fateless@vk.com>
2022-10-27 16:09:41 +0500

    Setup project.

--- 13th-HW/Sources/Views/SettingsViewController.swift
+++ 13th-HW/Sources/Views/SettingsViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  SettingsViewController.swift
 //  13th-HW
 //
 //  Created by Максим Солобоев on 27.10.2022.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class SettingsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
